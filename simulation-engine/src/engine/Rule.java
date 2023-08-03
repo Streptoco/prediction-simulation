fb13e5a2e2a311c48fb893a9dbf22fb5520ec505
Streptoco <81880440+Streptoco@users.noreply.github.com>
2023-08-03 11:59:39 +0300

    added classes

--- simulation-engine/src/engine/Rule.java
+++ simulation-engine/src/engine/Rule.java
@@ -1,7 +1,15 @@
 package engine;
 
+import java.util.ArrayList;
+
 public class Rule {
     private String name;
     private int tickIntervals;
     private double probability;
+    private ArrayList<Action> actions;
+
+    public boolean activation(int currentTickCount) {
+        //TODO: add timing and probability generator so that we could see if it should be activated.
+        return true;
+    }
 }
