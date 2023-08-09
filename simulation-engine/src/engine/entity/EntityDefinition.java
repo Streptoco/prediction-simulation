fc0b496863ab4c268ee834855826000115850d65
Streptoco <81880440+Streptoco@users.noreply.github.com>
2023-08-09 15:51:54 +0300

    big balagan

--- simulation-engine/src/engine/entity/EntityDefinition.java
+++ simulation-engine/src/engine/entity/EntityDefinition.java
@@ -1,23 +1,25 @@
 package engine.entity;
 
+import engine.properties.api.AbstractProperty;
+
 import java.util.ArrayList;
 
 public class EntityDefinition {
     private String name;
     private int population;
-    private ArrayList<Property> propertyList;
+    private ArrayList<AbstractProperty> propertyList;
 
     public EntityDefinition(String name, int population){
         this.name = name;
         this.population = population;
     }
 
-    public void setProperties(ArrayList<Property> propertyList) {
+    public void setProperties(ArrayList<AbstractProperty> propertyList) {
         this.propertyList = propertyList;
     }
 
-    public Property getPropertyByName(String propertyName) {
-        for (Property property : propertyList) {
+    public AbstractProperty getPropertyByName(String propertyName) {
+        for (AbstractProperty property : propertyList) {
             if (property.getName().equals(propertyName)) {
                 return property;
             }
