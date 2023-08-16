2b9f6b0b757ce67ef49aa9219170bc2c54dc510f
afikat123 <96895559+afikat123@users.noreply.github.com>
2023-08-16 13:56:31 +0300

    maybe the condition is working

--- simulation-engine/src/engine/actions/impl/condition/impl/MultipleCondition.java
+++ simulation-engine/src/engine/actions/impl/condition/impl/MultipleCondition.java
@@ -1,14 +1,12 @@
 package engine.actions.impl.condition.impl;
 
-import engine.actions.expression.Expression;
-
 import java.util.Arrays;
 import java.util.List;
 
 public class MultipleCondition implements Condition {
     List<Condition> conditionList;
     LogicalOperatorForSingularity logicalOperator;
-    public MultipleCondition(String logicalOperator, Condition... conditions) {
+    public MultipleCondition(String logicalOperator, List<Condition> conditions) {
         this.logicalOperator = logicalOperator.equalsIgnoreCase("and") ? LogicalOperatorForSingularity.AND : logicalOperator.equalsIgnoreCase("or") ? LogicalOperatorForSingularity.OR : null;
         this.conditionList = Arrays.asList(conditions);
     }
--- simulation-engine/src/engine/worldbuilder/factory/ConditionFactory.java
+++ simulation-engine/src/engine/worldbuilder/factory/ConditionFactory.java
@@ -3,6 +3,7 @@ package engine.worldbuilder.factory;
 import engine.actions.api.ActionInterface;
 import engine.actions.expression.Expression;
 import engine.actions.impl.condition.impl.Condition;
+import engine.actions.impl.condition.impl.MultipleCondition;
 import engine.actions.impl.condition.impl.SingleCondition;
 import engine.worldbuilder.prdobjects.PRDCondition;
 
@@ -11,14 +12,22 @@ import java.util.List;
 
 public class ConditionFactory{
     public static Condition BuildCondition (PRDCondition prdCondition) {
-        List<Condition> conditionList = new ArrayList<>();
-        if(prdCondition.getSingularity().equalsIgnoreCase("single")) {
-            conditionList.add(new SingleCondition(prdCondition.getProperty(), prdCondition.getOperator(), new Expression(prdCondition.getValue())));
-        }
-
-        else if (prdCondition.getSingularity().equalsIgnoreCase("multiple")) {
-
+            if (prdCondition.getSingularity().equalsIgnoreCase("single")) {
+                return (new SingleCondition(prdCondition.getProperty(), prdCondition.getOperator(), new Expression(prdCondition.getValue())));
+            } else if (prdCondition.getSingularity().equalsIgnoreCase("multiple")) {
+                return new MultipleCondition(prdCondition.getLogical(), BuildConditionFromList(prdCondition.getPRDCondition()));
             }
-        }
+            else {
+                return null;
+            }
     }
+
+    public static List<Condition> BuildConditionFromList(List<PRDCondition> prdConditionList) {
+        List<Condition> conditionList = new ArrayList<>();
+        for(PRDCondition currentCondition : prdConditionList) {
+            conditionList.add(BuildCondition(currentCondition));
+        }
+        return conditionList;
+    }
+
 }
