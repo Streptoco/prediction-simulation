015e6f5958ee1596a4540ffec80a520a92c1c3fd
Streptoco <81880440+Streptoco@users.noreply.github.com>
2023-08-19 15:39:56 +0300

    simulation running seamlessly

--- ui/src/driver.java
+++ ui/src/driver.java
@@ -1,4 +1,6 @@
+import engine.general.object.Engine;
 import exceptions.FileDoesNotExistException;
+import uitoengine.filetransfer.FileTransferDTO;
 
 import java.io.File;
 import java.util.Scanner;
@@ -10,8 +12,11 @@ public class driver {
 
     private static void WelcomePlayer() {
         System.out.println("Hello and welcome to the predictions simulator!\nPlease enter a valid XML file path.");
+        Engine engine = new Engine();
         Scanner scanner = new Scanner(System.in);
         String filePath = scanner.nextLine();
+        //TODO: check engine.setXmlFilePath()
+        
     }
     private static void Run() {
         WelcomePlayer();
