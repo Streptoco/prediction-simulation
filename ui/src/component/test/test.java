a017b23cf21b14635a959863c7e1da5812ddd822
Streptoco <81880440+Streptoco@users.noreply.github.com>
2023-08-26 21:21:51 +0300

    all files work

---
+++ ui/src/component/test/test.java
@@ -0,0 +1,32 @@
+package component.test;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class test extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the windows");
+
+        FXMLLoader loader = new FXMLLoader();
+        URL url = getClass().getResource("test.fxml");
+        loader.setLocation(url);
+        Parent root = loader.load();
+        testController testController = loader.getController();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+}
