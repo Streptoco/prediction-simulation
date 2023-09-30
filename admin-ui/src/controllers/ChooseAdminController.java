2c3f2ac4693f6a4800cf45a30f83432c69d2b191
afikat123 <96895559+afikat123@users.noreply.github.com>
2023-09-30 19:16:12 +0300

    start client ui

--- admin-ui/src/controllers/ChooseAdminController.java
+++ admin-ui/src/controllers/ChooseAdminController.java
@@ -1,7 +1,11 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,5 +13,10 @@ public class ChooseAdminController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void onFileLoad(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(new Stage());
     }
 }
