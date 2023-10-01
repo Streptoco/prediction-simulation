b8d91e5c28438f1e8dfed44e896511d0bca39935
Streptoco <81880440+Streptoco@users.noreply.github.com>
2023-10-01 18:41:28 +0300

    alomst there hehe

--- server/src/InitializeServer.java
+++ server/src/InitializeServer.java
@@ -1,7 +1,7 @@
 import engine.general.object.Engine;
-import jakarta.servlet.ServletContextEvent;
-import jakarta.servlet.ServletContextListener;
-import jakarta.servlet.annotation.WebListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 @WebListener
 public class InitializeServer implements ServletContextListener {
@@ -9,6 +9,11 @@ public class InitializeServer implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
 //        Engine engine = new Engine();
         System.out.println("Starting Predictions Server");
-        //sce.getServletContext().setAttribute("engine", new Engine());
+        sce.getServletContext().setAttribute("engine", new Engine());
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        // hey lol
     }
 }
